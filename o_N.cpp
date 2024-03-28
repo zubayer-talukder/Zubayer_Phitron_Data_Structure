767b8349263a5fe592c0cde53fedacff9cc56610
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-03-29 01:05:48 +0600

    full code

--- o_N.cpp
+++ o_N.cpp
@@ -3,6 +3,19 @@ using namespace std;
 int main()
 {
     int n;
-       
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+
+        //ei loop er complexity holo--> o(n)
+    }
+    int s=0;
+    for(int i=0;i<n;i++){
+        s+=a[i];
+//ei loop er complexity holo--> o(n)
+    }
+    // so total program er complexity holo --> o(n)
+cout<<s<<endl;
 return 0;
 }
