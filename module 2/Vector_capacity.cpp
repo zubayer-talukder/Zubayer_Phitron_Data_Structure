196ea13867da0fc660509dd10efb0d7507e0b521
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-03 15:22:19 +0600

    Vector capacity

---
+++ module 2/Vector_capacity.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // // case:1
+    // // V.max_size()--->> vector  er maximum size gula dekhabe
+    // vector<int> V;
+    // cout << V.max_size() << endl;
+
+    // // output
+    // // 1073741823
+
+//     // case:2
+//     // V.capacity()--->> vector  er maximum koto gula store kore oita dekhabe
+
+//     // vector<int> V;
+//     // cout << V.capacity() << endl;
+//     // // vector er kisu nai tai default e value 0 dekhay
+
+//     // value barano holo akn . Note: Vector 2 gun kore value increase kore 
+
+//     vector<int> V;
+//     V.push_back(23);
+//     cout << V.capacity() << endl;
+//     // output 1
+
+//     V.push_back(43);
+//     cout << V.capacity() << endl;
+// // output 1x2=2
+// V.push_back(3);
+//     cout << V.capacity() << endl;
+//     // output 2x2=4
+   
+
+//     V.push_back(2);
+//     cout << V.capacity() << endl;
+//     // output 4
+// // highest 4 ta vlue rakhte pare but valu ase  4 ta 
+
+// // eibar value 5 ta korley value abar increase korbe and capacity 8 hoye jabe
+
+// V.push_back(75);
+//     cout << V.capacity() << endl;
+//      // output 4x2=8
+    return 0;
+}
