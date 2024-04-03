f953a9deb04938363f9f8d53b59ee2de7d265a3d
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-03 10:56:07 +0600

    O_SqrtN is Fnished

--- module 1/O_sqrtN.cpp
+++ module 1/O_sqrtN.cpp
@@ -1,15 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=sqrt(n);i++){
-        if(n%i==0){
-        cout<<i<< " " ;
-        if(n/i!=i)cout<<n/i<<endl;
+    cin >> n;
+    // for(int i=1;i<=sqrt(n);i++){
+    //     if(n%i==0){
+    //     cout<<i<<" x ";
+    //     if(n/i!=i)cout<<n/i<<endl;
 
-    }}
+    // this code can be written like this
+    for (int i = 0; i * i <= n; i++)
+    {
+        cout << i << endl;
+        // this code is also example of // o(sqrt(n)) time complexity
+    }
 
-return 0;
+    return 0;
 }
