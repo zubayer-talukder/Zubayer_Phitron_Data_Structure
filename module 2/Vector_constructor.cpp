326143c93f659a7772c0dc13dd4515716213c111
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-03 14:54:11 +0600

    vector Constructor

--- module 2/Vector_constructor.cpp
+++ module 2/Vector_constructor.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -6,54 +6,56 @@ int main()
 
     // vector<int> v;
     // cout<<v.size()<<endl;
-    
+
     // // default vector size is zero
 
+    // case 2: Construct a vector  with N elements and the value will be garbage
 
-// case 2: Construct a vector  with N elements and the value will be garbage
+    //  vector<int > v(5);
+    // cout<<v.size()<<endl;
+    // // ei khane size 5 hoise
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<" ";
+    //     // size 5 hoise thik ase but each and every value hisebe default value ase 0
 
-//  vector<int > v(5);
-// cout<<v.size()<<endl;
-// // ei khane size 5 hoise
-// for(int i=0;i<v.size();i++){
-//     cout<<v[i]<<" ";
-//     // size 5 hoise thik ase but each and every value hisebe default value ase 0
+    //     // output ase
+    //     // 0 0 0 0 0
 
-//     // output ase
-//     // 0 0 0 0 0
+    // }
 
-// }
+    // case 3: Construct a vector  with N elements and the  value will be x
+    // Syantax::  vector<type> V (N,x)
 
-// case 3: Construct a vector  with N elements and the  value will be x
-// Syantax::  vector<type> V (N,x)
+    // vector<int > v(5,29);
+    // cout<<v.size()<<endl;
+    // // ei khane size 5 hoise
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<" ";
+    //     // size 5 hoise thik ase but each and every value hisebe  value ase 29
 
-// vector<int > v(5,29);
-// cout<<v.size()<<endl;
-// // ei khane size 5 hoise
-// for(int i=0;i<v.size();i++){
-//     cout<<v[i]<<" ";
-//     // size 5 hoise thik ase but each and every value hisebe  value ase 29
+    //     // output asce
+    //     // 5
+    //     // 29 29 29 29 29
+    // }
 
-//     // output asce
-//     // 5
-//     // 29 29 29 29 29
-// }
+    // case 4: Construct a vector  by copying another vector  X2
 
-// case 4: Construct a vector  by copying another vector  X2
+    // Syntax--::
+    // vector<type> V(X2)
 
-// Syntax--:: 
-// vector<type> V(X2)
+    vector<int> v(6, 100);
+    vector <int >X(v);
 
-vector<int > v(5,29);
-cout<<v.size()<<endl;
-// ei khane size 5 hoise
-for(int i=0;i<v.size();i++){
-    cout<<v[i]<<" ";
-    // size 5 hoise thik ase but each and every value hisebe  value ase 29
+        cout << X.size() << endl;
 
-    // output asce
-    // 5
-    // 29 29 29 29 29
-}
-return 0;
+    for (int i = 0; i < X.size(); i++)
+    {
+        cout << X[i] << " ";
+        // size 6 hoise thik ase but each and every value hisebe  value ase 100  ja copy kore X vector eo same ase
+
+        // output asce
+        // 6
+        // 100 100 100 100 100 100 
+    }
+    return 0;
 }
