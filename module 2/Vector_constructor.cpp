03ac45581a30a46b997944884cd03237391ada65
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-03 15:06:43 +0600

    Vector Constructor

--- module 2/Vector_constructor.cpp
+++ module 2/Vector_constructor.cpp
@@ -38,24 +38,58 @@ int main()
     //     // 29 29 29 29 29
     // }
 
-    // case 4: Construct a vector  by copying another vector  X2
+    // // case 4: Construct a vector  by copying another vector  X2
 
-    // Syntax--::
-    // vector<type> V(X2)
+    // // Syntax--::
+    // // vector<type> V(X2)
 
-    vector<int> v(6, 100);
-    vector <int >X(v);
+    // vector<int> v(6, 100);
+    // vector <int >X(v);
 
-        cout << X.size() << endl;
+    //     cout << X.size() << endl;
 
-    for (int i = 0; i < X.size(); i++)
-    {
-        cout << X[i] << " ";
-        // size 6 hoise thik ase but each and every value hisebe  value ase 100  ja copy kore X vector eo same ase
+    // for (int i = 0; i < X.size(); i++)
+    // {
+    //     cout << X[i] << " ";
+    //     // size 6 hoise thik ase but each and every value hisebe  value ase 100  ja copy kore X vector eo same ase
 
-        // output asce
-        // 6
-        // 100 100 100 100 100 100 
-    }
+    //     // output asce
+    //     // 6
+    //     // 100 100 100 100 100 100
+    // }
+
+    //     // case 5: Construct a vector  by all elemnts from an array A of Size N
+
+    //     // soja kotha holo akta array theke copy kore akta vector banano
+
+    //     // Syntax--::
+    //     // vector<type> V(A,A+N)
+
+    //     int A[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+    //     // simple akta array
+
+    //     vector<int> X(A, A + 8);
+
+    //     cout << X.size() << endl;
+
+    //     for (int i = 0; i < X.size(); i++)
+    //     {
+    //         cout << X[i] << " ";
+    //         // size 6 hoise thik ase but each and every value hisebe  value ase 100  ja copy kore X vector eo same ase
+
+    //       }
+    //    // output asce
+    //     //     8
+    //     //     10 20 30 40 50 60 70 80
+
+    // //    case :6;
+    // //vector er array bananu
+
+    // vector<int> V={10,20,30,40};
+    // for(int i=0;i<V.size();i++){
+    //     cout<<V[i]<<" ";
+    // }
+    // // output : 10 20 30 40 
+    
     return 0;
 }
