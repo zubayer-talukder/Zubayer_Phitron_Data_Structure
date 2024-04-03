5724f38f52d444a643a1ff80d7c2ac25756eb60f
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-04-03 21:23:33 +0600

    iteration

--- module 2/Element_iterator.cpp
+++ module 2/Element_iterator.cpp
@@ -1,7 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
-return 0;
+    /* 
+    Element access Kore:
+
+
+    vector<int> p = {10, 25, 4, 59, 4, 47, 98, 36, 9};
+    cout << p.back() << endl;
+    cout << p[p.size() - 3] << endl; // last er theke 3 ta index er por print korbe
+
+    // last er index print korbe
+
+    cout << p[3] << endl;
+        // specific index  print korbe
+
+
+cout << p.front() << endl;// first  index print korbe 
+    return 0;
+    */
+
+
+//    Iteration
+vector<int> x={10,23,32,34,5};
+int n;
+cin>>n;
+for(int y=0;y<x.size();y++)
+{
+    cout<<x[y]<< " ";
+}
 }
