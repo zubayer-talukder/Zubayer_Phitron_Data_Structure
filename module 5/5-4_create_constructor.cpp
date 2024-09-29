67a82b3dea152c458dc80634b64de4651b5540e9
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-09-29 19:48:43 +0600

    Constuctor

---
+++ module 5/5-4_create_constructor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    // constructor makeing kora hosse
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    };
+};
+int main()
+{
+    /* there is no need to this below line because i have created a  constucture
+
+
+    Node a, b;
+     a.value = 20;
+     b.value = 50;
+
+     b.next = NULL;
+     */
+
+    /*Call the consturcture */
+    Node a(43);
+    Node b(32);
+    a.next = &b;
+    cout << a.value << endl;
+    cout << a.next->value << endl;
+    // it can be write this way also
+    cout << (*a.next).value << endl; // same result paici ami
+
+    // kind of dereferance kora : b er address e giya b er value print koro  ,
+    
+    cout << b.value << endl;
+    cout << b.next->value << endl;
+    
+    return 0;
+}
